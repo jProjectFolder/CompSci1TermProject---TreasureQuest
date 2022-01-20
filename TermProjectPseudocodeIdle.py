4468a42c07fa71be75a92fe557bbd2b193115987
jProjectFolder <97361552+jProjectFolder@users.noreply.github.com>
2022-01-20 12:40:43 -0600

    Add files via upload

---
+++ TermProjectPseudocodeIdle.py
@@ -0,0 +1,14 @@
+#   Call up turtle screen set to a certain size
+#   Call up a pop-up box that asks player to input level amount
+#   Once input received, the turtle is movable by key commands.
+#   A text box in corner shows how many moves remain.
+#   Each time a player moves, the movement-counter goes down.
+#   If the counter equals zero, a pop-up box appears with a randomly generated math question
+#       If the player answeres correctly, the question box disppears and more movement is added
+#       Else, the player gets a new randomly generated math question and loses one of his\her three lives
+#   This process loops until the player is within specified distance of the treasure point
+#   Once the treasure is found on a level, a new level begins with harder questions.
+#   A score box with:
+#       A score of total movements 
+#       A score of correct answers 
+#       A score for wrong answers 
